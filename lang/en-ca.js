79493bf0244f36e9548a7ebd0ffb231c036de5c1
nmmf <stefan.paulin@all-inkl.com>
2015-12-16 14:14:07 +0100

    Create en-ca.js

---
+++ lang/en-ca.js
@@ -0,0 +1,9 @@
+/*
+Translation from original CKEDITOR language files:
+Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang("nmmabortandsave","en-ca",{
+	"abort":"Abort",
+	"save":"Save"
+});
